bf6eb490ed2c6e8f37fa640a20e275512320af98
omayma-chakkour <oouummaa.sh@gmail.com>
2022-02-11 13:41:17 +0100

    hello shit

---
+++ try/test.java
@@ -0,0 +1,12 @@
+package try;
+
+public class test {
+
+    public static void name() {
+        System.out.println("shit");
+        System.out.println("shit 1");
+        System.out.println("shit 2");
+
+    }
+    
+}
